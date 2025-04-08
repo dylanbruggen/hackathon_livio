2b08890c6123ba279391a696cd94398d515c612a
dylan ter bruggen <0337452@student.rocvantwente.nl>
2025-04-08 09:57:52 +0200

    1

---
+++ Bongo/Bongo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bongo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
